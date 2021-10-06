c053f5c9d48d1559ad4ef538e3103615cfc95158
Vinh <onehitdragon@gmail.com>
2021-10-06 08:46:59 +0700

    tiep tuc lam register

--- register.php
+++ register.php
@@ -21,15 +21,18 @@
             <div>
                 <p class="title">Ngày sinh</p>
                 <div class="birthday">
-                    <input class="textbox" type="text">
-                    <p class="birthday__calendar"><i class="far fa-calendar"></i></p>                   
-                </div>               
+                    <input class="textbox" type="text" value="1/1/1900">
+                    <p class="birthday__calendar"><i class="far fa-calendar"></i></p>                       
+                </div>
+                <div class="date-picker">
+
+                </div>
             </div>
             <div>
                 <p class="title">Giới tính</p>
                 <div class="gender">
                     <p>
-                        <input type="radio" name="gender" value="male"> <span>Nam</span>
+                        <input type="radio" name="gender" value="male" checked> <span>Nam</span>
                     </p>
                     <p>
                         <input type="radio" name="gender" value="female"> <span>Nữ</span>
@@ -46,13 +49,29 @@
         <div class="register__row register__row--1">
             <div>
                 <p class="title">Mật khẩu</p>
-                <input class="textbox" type="text">
+                <input class="textbox" type="password">
+            </div>
+        </div>
+        <div class="register__row register__row--1">
+            <div>
+                <p class="title">Nhập lại mật khẩu</p>
+                <input class="textbox" type="password">
             </div>
         </div>
         <div class="register__row register__row--1">
             <div>
                 <p class="title">Số điện thoại</p>
                 <input class="textbox" type="text">
+            </div>
+        </div>
+        <div class="register__row register__row--1">
+            <div>
+                <input class="cb-policy" type="checkbox" name="agreenPolicy"><span> Tôi đồng ý với <a>điều khoản</a> & <a>chính sách</a></span>
+            </div>
+        </div>
+        <div class="register__row register__row--1">
+            <div>
+                <button type="submit" class="agree">Đăng ký</button>
             </div>
         </div>
     </form>   
