ea54a6df887c1a253c2a1905adc8caf29b01c971
quoc3113 <pvuongquoc5@gmail.com>
2021-10-17 21:03:44 +0700

    fTTCN

--- chat.php
+++ chat.php
@@ -1,5 +1,15 @@
 <?php
         require './backend/web_config.php';
         load_top();
-        load_footer();
+
 ?>
+<div class="container">
+        <h2 id="about">ZiZi Chat</br></h2>
+        <div class="login-form col-4">
+            <form method="$_POST" action="./pro.php">
+
+                <input  class="btnLogin btn btn-outline-primary" type="submit" name="" value="TTCN"><br>
+            </form>
+        </div>
+</div>
+<?php load_footer(); ?>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid justify-content-start">
-                <a href="#" class="navbar-brand mb-0 h1">LOGO</a>
+                <a href="#" class="navbar-brand mb-0 h1">ZIZI Chat</a>
             </div> 
            
             <form class="log-re flex-d col-sm-2">
@@ -17,12 +17,13 @@
 <div class="container">
         <h2 id="about">Kết nối<br>với mọi người <br>thật dễ dàng</br></h2>
         <div class="login-form col-4">
-            <form method="$_POST" action="./chat.php">
+            <form method="$_POST" action="./profile.php">
                 <input id="username" class="form-control" type="text" placeholder="Nhập tài khoản"><br>
                 <input id="password" class="form-control" type="password" placeholder="Nhập mật khẩu"><br>
-                <input class="btnLogin btn btn-outline-primary" type="button" name="" value="Đăng Nhập"><br>
+                <input  class="btnLogin btn btn-outline-primary" type="submit" name="" value="Đăng Nhập"><br>
             </form>
         </div>
             <a href="./register.php" class="reg-link nav-link">Chưa có tài khoản? Đăng ký ngay</a>
 </div>
+
    
