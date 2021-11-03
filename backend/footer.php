367e5239122a453505ee70c0d5184e303314316f
unknown <dopagovn@gmail.com>
2021-11-03 19:48:54 +0700

    Update chat.php

--- backend/footer.php
+++ backend/footer.php
@@ -1,12 +1,3 @@
-<style>
-.footer {
-    position:fixed;
-    height: 30px;
-    width: 100%;
-    clear: both;
-    bottom: 0;
-}
-</style>
 
 <!--Add Boostrap JS-->
 <script src="./frontend/bootstrap/js/bootstrap.bundle.min.js"></script>
